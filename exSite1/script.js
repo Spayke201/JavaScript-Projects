7b6a1566995cfbd3874883e3260627edf63e4f5b
João Vitor M. Gimenes <52357255+Spayke201@users.noreply.github.com>
2020-07-16 02:59:56 -0300

    final changed

--- exSite1/script.js
+++ exSite1/script.js
@@ -1,1 +1,9 @@
-alert('Ola!')
+
+function calcularHora () {
+    var titulo = window.document.getElementById('titulo');
+    var data = new Date();
+    var hora = data.getHours();
+    var minutos = data.getMinutes();
+    console.log(typeof(minutos), minutos);
+    titulo.innerHTML = `Agora são ${hora}:${minutos} horas.`;
+}
